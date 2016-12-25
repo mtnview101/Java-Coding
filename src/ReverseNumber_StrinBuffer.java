d92a517c58ea70a97f54ff0d2c3e6230de4ed9e7
mtnview101 <mtnview101@gmail.com>
2016-12-24 16:13:22 -0800

    reverse number commit

--- src/ReverseNumber_StrinBuffer.java
+++ src/ReverseNumber_StrinBuffer.java
@@ -1,13 +1,17 @@
-
 public class ReverseNumber_StrinBuffer {
 public static void main(String[] args) {
-	int number=12345;
-	String number_string=String.valueOf(number);
-	StringBuffer string_buffer=new StringBuffer(number_string);
-	string_buffer.reverse();
 	
-	String reversed_string=string_buffer.toString();
-	int reverse=Integer.parseUnsignedInt(reversed_string);
+	int number=12345;
+										//String number_string=String.valueOf(number);
+										//StringBuffer string_buffer=new StringBuffer(number_string);
+	StringBuffer string_buffer=new StringBuffer(String.valueOf(number));
+										//string_buffer.reverse();
+										//String reversed_string=string_buffer.toString();
+										//String reversed_string=string_buffer.reverse().toString();
+										//int reverse=Integer.parseUnsignedInt(reversed_string);
+	int reverse=Integer.parseUnsignedInt(string_buffer.reverse().toString());	
+	
 	System.out.println("Reverse: "+reverse);
 }
 }
+
